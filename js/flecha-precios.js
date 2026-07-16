12acbc733ae1aa4abeb56a54767b0ef79fcc4130
carlosant1991 <antodomicz2013@gmail.com>
2026-07-16 12:23:00 -0500

    Create flecha-precios.js

---
+++ js/flecha-precios.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  setTimeout(()=>{
+    const e=document.getElementById("flecha-precios");
+    if(e)e.classList.add("visible");
+  },10000);
+});
