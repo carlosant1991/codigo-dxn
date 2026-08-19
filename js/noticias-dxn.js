14b46e51dec13e9a475f35d45f69d1fead0f162a
Carlos Antonio <antodomicz2013@gmail.com>
2026-08-19 15:23:01 -0500

    Update noticias-dxn.js

--- js/noticias-dxn.js
+++ js/noticias-dxn.js
@@ -1,9 +1,21 @@
 (function(){
-  'use strict';
+'use strict';
 
-  var track=document.getElementById('dxn-news-track');
-  if(!track)return;
+var track=document.getElementById('dxn-news-track');
+if(!track)return;
 
-  var items=track.innerHTML;
-  track.innerHTML=items+items;
+var items=track.querySelectorAll('.dxn-news-item');
+if(!items.length)return;
+
+var html=track.innerHTML;
+track.insertAdjacentHTML('beforeend',html);
+
+track.addEventListener('touchstart',function(){
+track.style.animationPlayState='paused';
+},{passive:true});
+
+track.addEventListener('touchend',function(){
+track.style.animationPlayState='';
+},{passive:true});
+
 })();
